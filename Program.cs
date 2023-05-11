6014cf88bfa9261f9f9802697f5a2f6f0d6933b4
Ayoub SAHI <ayoubsahi@outlook.com>
2023-05-11 12:42:33 +0000

    init

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ClosedXML.Excel;
+
+Console.WriteLine("Hello, World!");
+
+using var workbook = new XLWorkbook("Decompte.xlsx");
+
+var worksheet = workbook.Worksheets.Worksheet("Decompte");
+
+var marche = worksheet.Cell("C6").Value;
+
+//worksheet.Cell("A2").FormulaA1 = "=MID(A1, 7, 5)";
+Console.WriteLine(marche);
+
+//workbook.SaveAs("HelloWorld.xlsx");
